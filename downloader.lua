5220d4f0702482067295e930b1818bea66c0c4c9
Markus Gachnang <markus.gachnang@hsr.ch>
2019-10-26 13:19:14 +0200

    -

--- downloader.lua
+++ downloader.lua
@@ -17,7 +17,7 @@ local files = {
     { url = urlBase .. "gachLib/state.lua", path = "./gachLib/state.lua"},
     { url = urlBase .. "gachLib/transposer.lua", path = "./gachLib/transposer.lua"},
     { url = urlBase .. "gachLib/util.lua", path = "./gachLib/util.lua"},
-    { url = urlBase .. "gachLib/type/init.lua", path = "./gachLib/type/init.lua"}
+    { url = urlBase .. "gachLib/type/init.lua", path = "./gachLib/type/init.lua"},
     { url = urlBase .. "gachLib/type/eventHandler.lua", path = "./gachLib/type/eventHandler.lua"}
   ,--},
   --woot = {
