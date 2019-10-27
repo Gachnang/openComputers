b08a573950e2ddec596337de701d912fae09d153
Markus Gachnang <markus.gachnang@hsr.ch>
2019-10-27 13:21:28 +0100

    Update controller.lua

--- controller/controller.lua
+++ controller/controller.lua
@@ -15,17 +15,21 @@ application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D
 local horizontalList = application:addChild(GUI.list(34, 2, 100, 3, 2, 0, 0xE1E1E1, 0x4B4B4B, 0xE1E1E1, 0x4B4B4B, 0x696969, 0xFFFFFF, true))
 horizontalList:setDirection(GUI.DIRECTION_HORIZONTAL)
 horizontalList:setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_TOP)
+
 horizontalList:addItem("Übersicht").onTouch = function()
 	selectTab(1)
 end
 horizontalList:addItem("Power").onTouch = function()
 	selectTab(2)
 end
-horizontalList:addItem("Woot").onTouch = function()
+horizontalList:addItem("RefinedStorage").onTouch = function()
 	selectTab(3)
 end
-horizontalList:addItem("Debug").onTouch = function()
+horizontalList:addItem("Woot").onTouch = function()
 	selectTab(4)
+end
+horizontalList:addItem("Debug").onTouch = function()
+	selectTab(5)
 end
 
 function selectTab(id)
